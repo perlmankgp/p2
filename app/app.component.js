52e1d2f586a304449dd0964248e3e2248d3fab43
Adam Perlman <perlmankgp@outlook.com>
2017-06-14 22:59:39 -0400

    modular architecture setup - stage 1

---
+++ app/app.component.js
@@ -0,0 +1,14 @@
+// app.component.js
+export const AppComponent = {
+  template: `
+    <header>
+        Bookmark Manager
+    </header>
+    <div>
+        <div ui-view></div>
+    </div>
+    <footer>
+        Copyright Bookmark Manager App 2017.
+    </footer>
+  `
+};
